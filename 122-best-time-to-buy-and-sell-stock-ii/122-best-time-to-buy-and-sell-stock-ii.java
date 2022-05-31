ae903e36f5a119353f8012637cacd921401aa260
birolguldal <46013256+birolguldal@users.noreply.github.com>
2022-05-31 17:28:39 -0400

    Time: 1 ms (93.16%), Space: 43.8 MB (67.61%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,15 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
         
+        //for loop 
+        //bir gun ve bir sonraki gun arasindaki fark ile cevabi bulabiliriz
+        
         int profit = 0;
         
-        for(int i = 1; i < prices.length; i++) {
-            if(prices[i] > prices[i - 1]) {
-                profit += prices[i] - prices[i - 1];
+        for(int i = 0; i < prices.length - 1; i++) {
+            if(prices[i] < prices[i + 1]) { 
+                profit += prices[i + 1] - prices[i];
+                
             }
         }
-        
-        return profit;
-        
+        return profit; 
     }
 }
