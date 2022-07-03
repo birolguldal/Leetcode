7e8d40e174eca2d9ba8cde4c8ff27d73670f46a7
birolguldal <46013256+birolguldal@users.noreply.github.com>
2022-07-03 17:35:15 -0400

    Time: 3 ms (97.76%), Space: 50.8 MB (91.78%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+    
+        if(grid == null || grid.length == 0) {
+            return 0;
+            
+        }
+        
+        int numIslands = 0;
+        
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] == '1') {
+                    numIslands += getIslanCount(grid,i,j);                    
+}
+            }
+        }
+        return numIslands;
+    }
+    
+    public int getIslanCount(char[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
+            return 0;
+        }
+    
+        grid[i][j] = '0';
+        getIslanCount(grid,i+1,j);
+        getIslanCount(grid, i - 1, j);
+        getIslanCount(grid, i, j + 1);
+        getIslanCount(grid, i, j - 1);
+        return 1;
+
+        
+    }
+}
