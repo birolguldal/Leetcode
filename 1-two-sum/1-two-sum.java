4112a9630cee7f05176a8d3d011113241d448896
birolguldal <46013256+birolguldal@users.noreply.github.com>
2022-08-10 23:54:16 -0400

    Time: 114 ms (13.50%), Space: 45 MB (60.80%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -2,14 +2,14 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         
         for(int i = 0; i < nums.length; i++) {
-            for(int j = i+1; j < nums.length; j++){
-            int res = target - nums[i];
-                if(res == nums[j]) {
+            for(int j = i + 1; j < nums.length; j++) {
+                int delta = target - nums[i];
+                if(delta == nums[j]) {
                     return new int[] {i,j};
-                }
+                }   
+            }
         }
-        }
-        
-        throw new IllegalArgumentException("No number Found");
+              throw new IllegalArgumentException("connot find");
+
     }
 }
