e716b0b7b4003204a6b687a094d3f48cc9d5d20c
birolguldal <46013256+birolguldal@users.noreply.github.com>
2022-08-10 23:13:22 -0400

    Time: 28 ms (17.06%), Space: 72.4 MB (11.35%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -3,13 +3,10 @@ class Solution {
         
         Arrays.sort(nums);
         
-        for(int i = 0; i < nums.length - 1; i++) {
-            if(nums[i] == nums[i+1]) {
+        for(int i = 0; i < nums.length -1; i++)
+            if(nums[i] == nums[i + 1])
                 return true;
-            }
-        }
-                    return false;      
-
-        
+            return false;
     }
+    
 }
