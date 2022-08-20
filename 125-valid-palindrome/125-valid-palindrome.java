68806aca9caa94a6120f9952578457354edb7daa
birolguldal <46013256+birolguldal@users.noreply.github.com>
2022-08-20 16:18:10 -0400

    Time: 6 ms (67.15%), Space: 43.4 MB (79.15%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,11 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
-        //convert to lower case letters and remove non-alhanumeric
-        //start to end pointers
-        
         s = s.toLowerCase();
-            
+        
         int start = 0;
         int end = s.length() - 1;
         
@@ -13,12 +10,14 @@ class Solution {
             
             while(start < end && !Character.isLetterOrDigit(s.charAt(start)))
                 start++;
-              while(start < end && !Character.isLetterOrDigit(s.charAt(end)))
-                end--;     
+             while(start < end && !Character.isLetterOrDigit(s.charAt(end)))
+                end--;
             
-           if(s.charAt(start++) != s.charAt(end--))
-          return false;
-      }
-      return true;
+            if(s.charAt(start++) != s.charAt(end--))
+                return false;            
+        }
+        
+        return true;
+        
     }
 }
