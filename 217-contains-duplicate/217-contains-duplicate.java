b129e8580e7da69ac81328ddf0f696fe0c452cfb
birolguldal <46013256+birolguldal@users.noreply.github.com>
2022-08-23 23:36:40 -0400

    Time: 10 ms (78.35%), Space: 70.7 MB (35.82%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -4,14 +4,16 @@ class Solution {
         HashSet<Integer> hs = new HashSet<>();
         
         for(int i = 0; i < nums.length; i++) {
-            
-            if(hs.contains(nums[i])) {
+            if(!hs.contains(nums[i])) {
+                hs.add(nums[i]);
+            } else {
                 return true;
             }
             
-            hs.add(nums[i]);
+           
         }
         
         return false;
+        
     }
 }
