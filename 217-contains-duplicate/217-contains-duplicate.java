1669bfc0c599d13938f592c724e1f6c1e3125bb6
birolguldal <46013256+birolguldal@users.noreply.github.com>
2022-08-24 00:09:41 -0400

    Time: 23 ms (28.10%), Space: 69.5 MB (54.00%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -6,8 +6,9 @@ class Solution {
         for(int i = 0; i < nums.length - 1; i++) {
             if(nums[i] == nums[i+1]) {
                 return true;
-            }
+            } 
         }
+        
         return false;
     }
 }
