9791ac8364201303e11e090549a90ee97a9a5956
birolguldal <46013256+birolguldal@users.noreply.github.com>
2022-08-25 11:53:43 -0400

    Time: 0 ms (100.00%), Space: 43.2 MB (17.50%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        
+        int left = 0;
+        int right = rows* columns - 1;
+        
+        while(left <= right) {
+            int midpoint = left + (right - left) / 2;
+            int midpoint_element = matrix[midpoint/columns] [midpoint%columns];
+            if(midpoint_element == target) {
+                return true;
+            } else if (target < midpoint_element) {
+                right = midpoint - 1;
+            } else if(target > midpoint_element) {
+                left = midpoint + 1;
+            }
+        }
+        
+        return false;
+        
+        
+    }
+}
